78021a585f53c90750df3735cbe8154218a95330
SamuelBarbosaDev <samueloficial@protonmail.com>
2023-11-08 10:37:03 -0300

    tests: Esqueleto para testar regra de negócio

---
+++ api/tests/tests_views.py
@@ -0,0 +1,27 @@
+from rest_framework.test import APITestCase
+
+
+class TestBusinessRule(APITestCase):
+    def setUp(self):
+        pass
+
+    def test_sign_up_a_user(self):
+        pass
+
+    def test_login_a_user(self):
+        pass
+
+    def test_add_tasks(self):
+        pass
+
+    def test_list_tasks(self):
+        pass
+
+    def test_mark_task_as_done(self):
+        pass
+
+    def test_update_task(self):
+        pass
+
+    def test_delete_task(self):
+        pass
