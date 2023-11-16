8ca5d78e7babd49b08a5a530d03a9ee2aec93b70
Samuel <samueloficial@protonmail.com>
2023-11-16 09:39:49 -0300

    feat: Criando o serializer de `Task`

---
+++ api/serializer.py
@@ -0,0 +1,13 @@
+from .models import Task
+from rest_framework import serializers
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            'id',
+            'title',
+            'done',
+            'describe',
+        ]
