7c64efe9d06b870688301fb599c99f9b502421b0
Samuel <samueloficial@protonmail.com>
2023-11-16 09:45:43 -0300

    feat: Adicionando rotas `task_list` & `task_detail`

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import default
+from api.views import default, TaskList, TaskDetail
 
 urlpatterns = [
     path('', default, name='default'),
+    path('task/', TaskList.as_view(), name='task_list'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task_detail')
 ]
