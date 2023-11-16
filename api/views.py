fbf9630c83a3835a6022bfc4a998edc284250258
Samuel <samueloficial@protonmail.com>
2023-11-16 09:42:11 -0300

    feat: Adicionando views `TaskList` & `TaskDetail`

--- api/views.py
+++ api/views.py
@@ -1,5 +1,23 @@
+from .models import Task
 from django.http import JsonResponse
+from .serializer import TaskSerializer
+from rest_framework import (
+    generics,
+    permissions,
+)
 
 
 def default(request):
     return JsonResponse({'Funcionou': 1})
+
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
