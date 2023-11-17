4f83daccac0ae65faa8c37d409d6df69846b0bfd
Samuel <samueloficial@protonmail.com>
2023-11-17 13:04:05 -0300

    feat: adicionando `RegisterView`

--- auth/views.py
+++ auth/views.py
@@ -1,8 +1,19 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
 from rest_framework.permissions import AllowAny
-from auth.serializers import AuthTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from auth.serializers import (
+    AuthTokenObtainPairSerializer,
+    RegisterSerializer
+)
 
 
 class AuthTokenObtainPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = AuthTokenObtainPairSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
